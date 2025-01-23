0ea252653149c2a9a0f37e7bffd3be93c8401ba4
anup2134 <anup.s.bhoos@gmail.com>
2025-01-23 13:58:34 +0000

    add tech stack marquee

--- src/components/StackStrip.tsx
+++ src/components/StackStrip.tsx
@@ -1,8 +1,46 @@
 import { useRef, useEffect } from "react";
-// import React from "../assets/react.svg";
+import React from "../assets/react.svg";
 export default function StackStrip() {
-  const canRef = useRef(null);
+  const canRef = useRef<HTMLCanvasElement>(null);
+  function loadImage(src: string) {
+    return new Promise((resolve, reject) => {
+      let img = new Image();
+      img.src = src;
+      img.onload = () => resolve(img);
+      img.onerror = reject;
+    });
+  }
 
-  useEffect(() => {}, []);
-  return <canvas ref={canRef} className="w-full h-24" />;
+  useEffect(() => {
+    const canvas = canRef?.current ?? null;
+    if (!canvas) return;
+    canvas.width = canRef.current?.offsetWidth ?? 0;
+    canvas.height = 100;
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+
+    const mainFun = async () => {
+      const img = (await loadImage(React)) as HTMLImageElement;
+      const obj = {
+        img,
+        x: 0,
+        y: 10,
+        speed: 2,
+      };
+      const draw = (obj: {
+        img: HTMLImageElement;
+        x: number;
+        y: number;
+        speed: number;
+      }) => {
+        ctx.drawImage(obj.img, obj.x, obj.y, 80, 80);
+      };
+
+      //   const
+      draw(obj);
+    };
+
+    mainFun();
+  }, []);
+  return <canvas ref={canRef} className="w-full" />;
 }
